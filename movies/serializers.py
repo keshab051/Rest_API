e5b4fcb0fbfa4a0f1b4bc9884add4c0acbca9cf6
KeshabNeupane <keshabneupane061@gmail.com>
2025-01-20 21:59:55 +0545

    serializing the data of movie

---
+++ movies/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Moviedata
+
+class Movieserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Moviedata
+        fields = ['id', 'name','duration','rating']
